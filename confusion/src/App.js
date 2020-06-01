50e377675265a58b65a3eb909de13bc74c206fa5
Shrey Tyagi <shreytyagi18@gmail.com>
2020-06-02 03:51:55 +0530

    Components Part 2

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -4,8 +4,17 @@ import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  
   render() {
     return (
       <div className="App">
@@ -14,7 +23,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
       
     );
