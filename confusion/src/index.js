ee2072082196aa5a27910e200caf154d69aaadf1
Shrey Tyagi <shreytyagi18@gmail.com>
2020-06-06 04:09:57 +0530

    Header and Footer

--- confusion/src/index.js
+++ confusion/src/index.js
@@ -4,6 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';   
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 
 ReactDOM.render(
   <React.StrictMode>
